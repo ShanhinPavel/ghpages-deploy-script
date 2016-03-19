5525c0feca0ede862ba187a327e8e702b053fd81
arlair <arlair@yahoo.com>
2016-03-19 13:57:46 +0800

    Update readme and attempt at using stash that isn't going to work, but save for posterity and remove in a bit

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -22,6 +22,17 @@ if [ "$cdup" != '' ]
 then
     git init
     git remote add --fetch origin "$remote"
+    # if we already have the site ready to push, but we can't checkout the new
+    # branch as it will say we have changes. So stash them and pop the stash
+    # later
+    if [[ `git status --porcelain` ]]; then
+        # changes
+        git stash
+        stashed=true;
+      else
+        # no changes
+        stashed=false;
+    fi
 fi
 
 if git rev-parse --verify origin/gh-pages > /dev/null 2>&1
@@ -29,6 +40,10 @@ then
     git checkout gh-pages
 else
     git checkout --orphan gh-pages
+fi
+
+if [ "$stashed" = true ] ; then
+  git stash pop
 fi
 
 # stage any changes and new files
